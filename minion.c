554a1b41fcdb3cdab5e42c76f9a23538d7236907
Amir Kooshky <kooshkya@gmail.com>
2024-12-12 10:03:31 -0500

    refactor minion.c

--- minion.c
+++ minion.c
@@ -16,8 +16,8 @@ struct timespec busyloop_start;
 
 int run_count;
 int stop_count;
-struct timespec runs[MAX_INTERVALS];
-struct timespec stops[MAX_INTERVALS];
+struct timespec run_intervals[MAX_INTERVALS];
+struct timespec stop_intervals[MAX_INTERVALS];
 
 void __always_inline custom_sleep(const struct timespec *duration) {
     struct timespec rem = {.tv_nsec = 0, .tv_sec = 0};
@@ -43,41 +43,57 @@ void __always_inline start_busyloop_clock() {
     clock_gettime(CLOCK_MONOTONIC, &busyloop_start);
 }
 
+int parse_arguments(int argc, char* argv[]) {
+    if (!(argc % 2)) {
+        printf("You need double numbers (secs nsecs) for each run/stop stint\n");
+        return 1;
+    }
+    if (argc == 1) {
+        printf("You need at least one run interval!\n");
+        return 2;
+    }
+    if (argc > MAX_INTERVALS * 4 + 1) {
+        printf("You can use at most %d run/stop doubles\n", MAX_INTERVALS);
+        return 3;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        switch(i % 4) {
+            case 1:
+                run_count++;
+                run_intervals[(i - 1) / 4].tv_sec = strtol(argv[i], NULL, 10);
+                break;
+            case 2:
+                run_intervals[(i - 2) / 4].tv_nsec = strtol(argv[i], NULL, 10);
+                break;
+            case 3:
+                stop_count++;
+                stop_intervals[(i - 3) / 4].tv_sec = strtol(argv[i], NULL, 10);
+                break;
+            case 0:
+                stop_intervals[(i - 4) / 4].tv_nsec = strtol(argv[i], NULL, 10);
+                break;
+        }
+    }
+    return 0;
+}
+
+void run_main_loop() {
+    for (int i = 0; i < run_count; i++) {
+        busyloop(&run_intervals[i], &busyloop_start);
+        if (i < stop_count)
+            custom_sleep(&stop_intervals[i]);
+        start_busyloop_clock();
+    }
+}
+
 int main(int argc, char *argv[]) {
     start_busyloop_clock();
+    int ret = parse_arguments(argc, argv);
+    if (ret)
+        return ret;
 
-    if (!(argc % 2))
-        printf("You need double numbers (secs nsecs) for each run/stop stint\n");
-    if (argc > MAX_INTERVALS * 4 + 1)
-        printf("You can use at most %d run/stop doubles\n", MAX_INTERVALS);
-
-    if (argc > 1) { // means program has been configured with run/stop intervals
-        for (int i = 1; i < argc; i++) {
-            switch(i % 4) {
-                case 1:
-                    run_count++;
-                    runs[(i - 1) / 4].tv_sec = strtol(argv[i], NULL, 10);
-                    break;
-                case 2:
-                    runs[(i - 2) / 4].tv_nsec = strtol(argv[i], NULL, 10);
-                    break;
-                case 3:
-                    stop_count++;
-                    stops[(i - 3) / 4].tv_sec = strtol(argv[i], NULL, 10);
-                    break;
-                case 0:
-                    stops[(i - 4) / 4].tv_nsec = strtol(argv[i], NULL, 10);
-                    break;
-            }
-        }
-
-        for (int i = 0; i < run_count; i++) {
-            busyloop(&runs[i], &busyloop_start);
-            if (i < stop_count)
-                custom_sleep(&stops[i]);
-            start_busyloop_clock();
-        }
-    } else {    // means program should run indefinitely
-        // TODO run indefinitely
-    }
+    run_main_loop();
+    
+    return 0;
 }
